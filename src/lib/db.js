f2bccab0cdfe44e1fc7cb1ace42a19bb70371608
Gabriel Bragion <gabriel.bragion.micco@gmail.com>
2025-02-27 14:45:53 +0000

    updated

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
   host: process.env.DB_HOSTNAME,
-  port: DB_PORT, 
+  port: process.env.DB_PORT, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
